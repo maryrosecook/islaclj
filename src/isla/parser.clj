06029f4ed0c3031601b4ead50cd8bb5c2dd05c61
Mary Rose Cook <maryrosecook@maryrosecook.com>
2012-06-15 15:36:16 -0400

    Handy comment.

--- src/isla/parser.clj
+++ src/isla/parser.clj
@@ -72,7 +72,7 @@
   (= symbol token))
 
 (defmethod is-type java.util.regex.Pattern [re token]
-  (if (keyword? token)
+  (if (keyword? token) ;; looking for string but potential is kw so not a match
     false
     (if (not= nil (re-matches re token))
       true
