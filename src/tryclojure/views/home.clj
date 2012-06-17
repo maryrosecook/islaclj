f121284e1300aabc0346f59cbc12e51ef95ceb3e
Mary Rose Cook <maryrosecook@maryrosecook.com>
2012-06-16 22:47:31 -0400

    Styling cleanup.

--- src/tryclojure/views/home.clj
+++ src/tryclojure/views/home.clj
@@ -3,37 +3,6 @@
             [hiccup.element :refer [javascript-tag link-to unordered-list]]
             [hiccup.page :refer [include-css include-js html5]]))
 
-(defpartial links []
-  (unordered-list
-   [(link-to "http://clojure.org" "The official Clojure website")
-    (link-to "http://dev.clojure.org/display/doc/Getting+Started" "Getting started with Clojure")
-    (link-to "http://groups.google.com/group/clojure" "Clojure mailing list")
-    (link-to "http://java.ociweb.com/mark/clojure/article.html" "A comprehensive Clojure tutorial")
-    (link-to "http://joyofclojure.com/" "The Joy of Clojure: a book by Michael Fogus and Chris Houser")
-    (link-to "http://disclojure.org" "Disclojure")
-    (link-to "http://planet.clojure.in" "Planet Clojure")]))
-
-(defpartial about-html []
-  [:p.bottom
-   "Please note that this REPL is sandboxed, so you wont be able to do everything in it "
-   "that you would in a local unsandboxed REPL. Keep in mind that this site is designed for "
-   "beginners to try out Clojure and not necessarily as a general-purpose server-side REPL."]
-  [:p.bottom
-   "One quirk you might run into is that things you bind with def can sometimes disappear. "
-   "The sandbox wipes defs if you def too many things, so don't be surprised. Furthermore, "
-   "The sandbox will automatically be wiped after 15 minutes and if you evaluate more after that,"
-   "It'll be in an entirely new namespace/sandbox."]
-  [:p.bottom
-   "You can find the site's source and such on its "
-   (link-to "http://github.com/Raynes/tryclojure" "github")
-   " page."]
-  [:p.bottom
-   "TryClojure is written in Clojure and JavaScript (JQuery), powered by "
-   (link-to "https://github.com/flatland/clojail" "clojail")
-   " and Chris Done's "
-   (link-to "https://github.com/chrisdone/jquery-console" "jquery-console")]
-  [:p.bottom "Design by " (link-to "http://apgwoz.com" "Andrew Gwozdziewycz")])
-
 (defpartial home-html []
   [:p.bottom
    ""])
