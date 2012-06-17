5b0525028b2740c91715b89499644cf5ad8f954b
Mary Rose Cook <maryrosecook@maryrosecook.com>
2012-06-16 21:40:29 -0400

    Set up Isla as a Noir project.

--- project.clj
+++ project.clj
@@ -1,4 +1,10 @@
-(defproject isla "0.1.0-SNAPSHOT"
-  :description "A programming language for children"
-  :dependencies [[org.clojure/clojure "1.3.0"]]
-  :main isla.cli)
+(defproject tryclojure "0.1.0-SNAPSHOT"
+  :description "A programming language for children."
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [noir "1.3.0-beta3"]
+		 [commons-lang/commons-lang "2.5"]
+                 [clojail "0.5.1"]]
+  :jvm-opts ["-Djava.security.policy=example.policy""-Xmx80M"]
+  :main tryclojure.server)
+
+
