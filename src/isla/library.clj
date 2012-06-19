dd05180460a0c3ca5cb063bf0d39c32f01e58ccf
Mary Rose Cook <maryrosecook@maryrosecook.com>
2012-06-19 15:12:53 -0400

    Allow addition of defrecords to Isla env.
    * These define 'types' that may be instantiated with `thing is a thingy`.
    * This instantiation creates the record for that type (which has slots and maybe methods).

--- src/isla/library.clj
+++ src/isla/library.clj
@@ -1,14 +1,19 @@
 (ns isla.library
   (:use [clojure.pprint])
   (:require [clojure.string :as str])
+  (:require [isla.user :as user])
   (:require [isla.utils :as utils]))
 
-(defn get-initial-env []
+(defn get-initial-env [& args]
+  (def extra-types (first args))
   {
    :ret nil
    :ctx {
          "write" (fn [env str]
                    (utils/output str) ;; print out
                    str) ;; add to context
-    }
+         }
+   :types (if (nil? extra-types)
+            user/types
+            (merge extra-types user/types))
   })
