8792d4c4a8be1e2cd55178b6be689015225666db
Mary Rose Cook <maryrosecook@maryrosecook.com>
2012-06-23 16:08:50 -0400

    Provide default Instrument type for testing.

--- src/isla/user.clj
+++ src/isla/user.clj
@@ -1,3 +1,9 @@
 (ns isla.user)
 
-(def types {})
+(defrecord Instrument [sound awesomeness])
+(def instrument-defaults ["" ""])
+
+(def types
+  {
+   "instrument" {:type isla.user.Instrument :defaults instrument-defaults}
+   })
