beb9d1ddf94b14d33bb829990632f6e2e330e4c2
Mary Rose Cook <maryrosecook@maryrosecook.com>
2012-06-23 16:18:01 -0400

    Remove unused code.

--- src/tryclojure/views/home.clj
+++ src/tryclojure/views/home.clj
@@ -4,10 +4,6 @@
             [hiccup.element :refer [javascript-tag link-to unordered-list]]
             [hiccup.page :refer [include-css include-js html5]]))
 
-(defpartial home-html []
-  [:p.bottom
-   ""])
-
 (defn root-html [mode title]
   (html5
    [:head
@@ -24,15 +20,6 @@
       [:div#container
        [:div#console.console]]
       (javascript-tag
-       "var _gaq = _gaq || [];
-        _gaq.push(['_setAccount', 'UA-27340918-1']);
-        _gaq.push(['_trackPageview']);
-
-        (function() {
-          var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-          ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-          var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-        })();")]]]))
        (str "mode = '" mode "';"))]]]))
 
 (defpage "/" []
