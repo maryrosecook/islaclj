bb30f02d1d2bb56a0600987bd54c2dfd9a0d0a1d
Mary Rose Cook <maryrosecook@maryrosecook.com>
2012-07-06 17:20:16 -0400

    Make lexer get rid of empty lines.

--- src/isla/lexer.clj
+++ src/isla/lexer.clj
@@ -6,12 +6,20 @@
   (vec (re-seq #"[^\s\"']+|\"[^\"]*\"|'[^']*'"
                        (str/trim line))))
 
+(defn chuck-empties [prev lines]
+  (if (> (count lines) 0)
+    (let [cur (first lines)]
+      (concat (if (and (= :nl cur) (= :nl prev)) [] [cur])
+              (chuck-empties cur (rest lines))))
+    []))
+
 (defn lex [string]
-  (let [tokens (flatten (map
-                         (fn [x] (conj x :nl))
-                         (map
-                          (fn [x] (split-a-line x))
-                          (str/split-lines string))))]
+  (let [tokens (chuck-empties nil
+                              (flatten (map
+                                        (fn [x] (conj x :nl))
+                                        (map
+                                         (fn [x] (split-a-line x))
+                                         (str/split-lines string)))))]
 
     ;; add final newline if required
     (if (not= :nl (last tokens))
