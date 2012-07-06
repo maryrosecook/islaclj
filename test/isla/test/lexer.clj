248d3584c48c074f4f294822fe8044fbf02583bb
Mary Rose Cook <maryrosecook@maryrosecook.com>
2012-07-06 17:20:28 -0400

    Add test for lexer dumping empty lines.

--- test/isla/test/lexer.clj
+++ test/isla/test/lexer.clj
@@ -18,5 +18,8 @@
 (deftest extra-whitespace
   (is (= (lex "   la    is   1  \n la   is    1  ") ["la" "is" "1" :nl "la" "is" "1" :nl])))
 
+(deftest test-multiple-newlines
+  (is (= (lex "la is 1\n\n\n la is 2") ["la" "is" "1" :nl "la" "is" "2" :nl])))
+
 (deftest test-keep-strings-intact
   (is (= (lex "name is 'isla eve'") ["name" "is" "'isla eve'" :nl])))
