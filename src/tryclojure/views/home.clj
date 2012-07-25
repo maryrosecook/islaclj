3981ac2c012516259dd7709427fabb7f8b428f2d
Mary Rose Cook <maryrosecook@maryrosecook.com>
2012-07-24 22:15:50 -0400

    Analytics.

--- src/tryclojure/views/home.clj
+++ src/tryclojure/views/home.clj
@@ -79,5 +79,15 @@
            <a href='http://github.com/maryrosecook/isla'>code</a> is on github.
           "]
          ]]
-       [:div.footer "by <a href='http://maryrosecook.com'>mary rose cook</a>"]]]]]))
+       [:div.footer "by <a href='http://maryrosecook.com'>mary rose cook</a>"]
+       (javascript-tag
+        (str "
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-24453347-2']);
+  _gaq.push(['_trackPageview']);
 
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();"))]]]]))
